70ba2cae73f7832f6141e35174a01fbfa47dadc2
Jon Eugster <eugster.jon@gmail.com>
2023-04-26 00:48:19 +0200

    correct def of Mul

--- NNG/MyNat/Multiplication.lean
+++ NNG/MyNat/Multiplication.lean
@@ -6,11 +6,11 @@ open MyNat
 
 def mul : MyNat → MyNat → MyNat
   | _, 0   => 0
-  | a, b + 1 => a + (MyNat.mul a b)
+  | a, b + 1 => (MyNat.mul a b) + a
 
 instance : Mul MyNat where
   mul := MyNat.mul
 
-axiom mul_zero (a : MyNat) : a * 0 = 0
+theorem mul_zero (a : MyNat) : a * 0 = 0 := by rfl
 
-axiom mul_succ (a b : MyNat) : a * (succ b) = a * b + a
+theorem mul_succ (a b : MyNat) : a * (succ b) = a * b + a := by rfl
