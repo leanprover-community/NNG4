134d5cc029171494e82c2f6d60ccf414f4268e99
Jon Eugster <eugster.jon@gmail.com>
2023-08-04 19:39:26 +0200

    bump gameserver

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ def LocalGameServer : Dependency := {
 
 def RemoteGameServer : Dependency := {
   name := `GameServer
-  src := Source.git "https://github.com/leanprover-community/lean4game.git" "f5eb185eb22d6ff3ade9639a2c818a0dc6abc930" "server"
+  src := Source.git "https://github.com/leanprover-community/lean4game.git" "5072dacf947d4f682e14a56816eb10f9e46d7203" "server"
 }
 
 /- Choose dependency depending on the environment variable NODE_ENV -/
