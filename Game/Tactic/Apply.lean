3fb6d7113fd807ce0d60068eb18be1f4dd59eaa6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-08-07 18:37:42 +0100

    apply to -> apply at

--- Game/Tactic/Apply.lean
+++ Game/Tactic/Apply.lean
@@ -4,18 +4,18 @@ open Lean Elab Tactic
 
 /--
 If `(h : A)` is a proof of `A` and `f : A → B` an implication then
-`apply f to h` turns `h` into a proof of `B`.
+`apply f at h` turns `h` into a proof of `B`.
 
-This is a game specific implementation. It is equivalent to the
-tactic `replace h := f h`.
+This is a game-specific implementation and not an official Lean tactic.
+It is equivalent to the tactic `replace h := f h`.
 -/
-syntax (name := applyTo) "apply" ident " to " ident : tactic
+syntax (name := applyAt) "apply" ident " at " ident : tactic
 
-elab_rules : tactic | `(tactic| apply $thm to $hyp) => do
+elab_rules : tactic | `(tactic| apply $thm at $hyp) => do
   evalTactic (← `(tactic| replace $hyp := $thm $hyp))
 
 -- Test
 example (A B C : Prop) (ha : A) (f : A → B) (g : B → C) : C := by
-  apply g
-  apply f to ha
-  assumption
+  apply f at ha
+  apply g at ha
+  exact ha
