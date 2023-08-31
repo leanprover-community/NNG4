d9249a6df3c4c7676d4723cb6ed7867d5f4083a4
Scott Morrison <scott.morrison@gmail.com>
2023-08-31 14:54:14 +1000

    bump toolchain

--- Game/MyNat/AdvAddition.lean
+++ Game/MyNat/AdvAddition.lean
@@ -4,7 +4,7 @@ namespace MyNat
 
 attribute [-simp] MyNat.succ.injEq
 example (a b : ℕ) (h : (succ a) = b) : succ (succ a) = succ b := by
-  simp
+  -- `simp` here makes no progress
   sorry
 
 axiom succ_inj {a b : ℕ} : succ a = succ b → a = b
