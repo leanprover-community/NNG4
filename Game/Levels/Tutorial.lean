7a25e8ca28d2b49fe1b6c4559c9ab9fa49ac50a3
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-09-21 15:54:55 +0100

    docstring for a random level tutorial, add TODO

--- Game/Levels/Tutorial.lean
+++ Game/Levels/Tutorial.lean
@@ -1,3 +1,5 @@
+/- **TODO** need that brief apache2 and author string?
+-/
 import Game.Levels.Tutorial.L01rfl
 import Game.Levels.Tutorial.L02rw
 import Game.Levels.Tutorial.L03three_eq_sss0
@@ -5,7 +7,15 @@ import Game.Levels.Tutorial.L04add_zero
 import Game.Levels.Tutorial.L05add_succ
 import Game.Levels.Tutorial.L06twoaddone
 import Game.Levels.Tutorial.L07twoaddtwo
+/-!
 
+# Tutorial world
+
+This file defines Tutorial World. Like all worlds, this world
+has a name, a title, an introduction, and most importantly
+a finite set of levels (imported above). Each level has a weighting
+and this determines the order of the levels.
+-/
 World "Tutorial"
 Title "Tutorial World"
 
