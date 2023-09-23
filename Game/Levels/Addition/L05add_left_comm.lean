6e7d01d64504dc2a61387587bf6e8b8f6265fe36
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-09-23 13:54:38 +0100

    Let's prove add_left_comm instead of add_right_comm

--- Game/Levels/Addition/L05add_left_comm.lean
+++ Game/Levels/Addition/L05add_left_comm.lean
@@ -8,30 +8,32 @@ namespace MyNat
 
 Introduction
 "
-`add_comm a b` is a proof that `a + b = b + a`. But if your goal
-is `a + (b + c) = b + (a + c)` then `rw [add_comm a b]` will not
-work, There is no `a + b` in this goal, because of the brackets.
+`add_comm b c` is a proof that `b + c = c + b`. But if your goal
+is `a + b + c = a + c + b` then `rw [add_comm b c]` will not
+work! Because the goal means `(a + b) + c = (a + c) + b` so there
+is no `b + c` term in the goal.
 
-`add_assoc` moves brackets around, and `add_comm` moves variables around.
-Use these theorems to solve this level.
-
-Remember that in Lean, `a + b + c` *means* `(a + b) + c`.
+Use associativity and commutativity to prove `add_right_comm`.
+You don't need induction. `add_assoc` moves brackets around,
+and `add_comm` moves variables around.
 "
 
-LemmaDoc MyNat.add_left_comm as "add_left_comm" in "Add"
-"`add_left_comm a b c` is a proof that `a + (b + c) = b + (a + c)`."
+LemmaDoc MyNat.add_right_comm as "add_right_comm" in "Add"
+"`add_right_comm a b c` is a proof that `(a + b) + c = (a + c) + b`.
+
+In Lean, `a + b + c` means `(a + b) + c`, so this result gets displayed
+as `a + b + c = a + c + b`."
 
 /-- If $a, b$ and $c$ are arbitrary natural numbers, we have
-$a + (b + c) = b + (a + c)$. -/
-Statement add_left_comm (a b c : ℕ) : a + (b + c) = b + (a + c) := by
-  Hint "To rewrite `h : X = Y` backwards (i.e. to change `Y`s to `X`s
-    rather than `X`s to `Y`s) use `rw [← h]`"
-  rw [← add_assoc]
+$(a + b) + c = (a + c) + b$. -/
+Statement add_right_comm (a b c : ℕ) : a + b + c = a + c + b := by
+  rw [add_assoc]
   Hint (hidden := true) "Remember that you can do more targetted rewrites by
-  adding explicit variables to theorems. For example `rw [add_comm b]` will only
-  do rewrites of the form `b + ? = ? + b`."
-  rw [add_comm a, add_assoc]
+  adding explicit variables as inputs to theorems. For example `rw [add_comm b]`
+  will only do rewrites of the form `b + ? = ? + b`."
+  rw [add_comm b, add_assoc]
   rfl
+
 LemmaTab "Add"
 
 Conclusion "
