f07062e9134742459e411f0b57ca65e9c4208cdc
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-09-25 19:51:14 +0100

    comment out error

--- Game/MyNat/AdvAddition.lean
+++ Game/MyNat/AdvAddition.lean
@@ -3,9 +3,9 @@ import Game.MyNat.Addition
 namespace MyNat
 
 attribute [-simp] MyNat.succ.injEq
-example (a b : ℕ) (h : (succ a) = b) : succ (succ a) = succ b := by
-  simp
-  sorry
+-- example (a b : ℕ) (h : (succ a) = b) : succ (succ a) = succ b := by
+--   simp
+--   sorry
 
 --axiom succ_inj {a b : ℕ} : succ a = succ b → a = b
 
