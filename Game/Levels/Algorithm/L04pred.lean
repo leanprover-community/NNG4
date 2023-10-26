5c36ad316b69a846554c4504f6c7032dcbe6d280
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-10-27 00:00:06 +0100

    Peano axiom levels

---
+++ Game/Levels/Algorithm/L04pred.lean
@@ -0,0 +1,47 @@
+import Game.Levels.Algorithm.L03add_algo2
+import Game.Levels.Implication
+
+World "Algorithm"
+Level 4
+Title "pred"
+
+LemmaTab "Peano"
+
+namespace MyNat
+
+Introduction
+"
+We now start work on an algorithm to do addition more efficiently. Recall that
+we defined addition by recursion, saying what it did on `0` and successors.
+It is an axiom of Lean that recursion is a valid
+way to define functions from types such as the naturals.
+
+Let's define a new function `pred` from the naturals to the naturals, which
+attempts to subtract 1 from the input. The definition is this:
+
+```
+pred 0 := 37
+pred (succ n) := n
+```
+
+We cannot subtract one from 0, so we just return a junk value. A new lemma `pred_succ`
+says that `pred (succ n) = n`. Let's use it to prove `succ_inj`, the theorem which
+Peano assumed as an axiom and which we have already used extensively without justification.
+"
+
+LemmaDoc pred_succ as "pred_succ" in "Peano"
+"
+`pred_succ n` is a proof of `pred (succ n) = n`.
+"
+
+/-- If $\operatorname{succ}(a)=\operatorname{succ}(b)$ then $a=b$. -/
+Statement (a b : ℕ) (h : succ a = succ b) : a = b := by
+  Hint "Start with `rw [← pred_succ a]` and take it from there."
+  rw [← pred_succ a]
+  rw [h]
+  rw [pred_succ]
+  rfl
+
+Conclusion
+"Let's now prove Peano's other axiom, `zero_ne_succ`.
+"
---
+++ Game/Levels/Algorithm/L05is_zero.lean
@@ -0,0 +1,66 @@
+import Game.Levels.Algorithm.L04pred
+
+World "Algorithm"
+Level 5
+Title "is_zero"
+
+LemmaTab "Peano"
+
+namespace MyNat
+
+Introduction
+"
+We now define a function `is_zero` thus:
+
+```
+is_zero 0 := True
+is_zero (succ n) := False
+```
+
+We also need two lemmas, `is_zero_zero` and `is_zero_succ`, saying that `is_zero 0 = True`
+and `is_zero (succ n) = False`. Let's use this function to prove `succ_ne_zero`, Peano's
+Last Axiom. We have been using `zero_ne_succ` before, but it's handy to have
+this opposite version too.
+
+If you can turn your goal into `True`, then the `tauto` tactic will solve it.
+"
+
+LemmaDoc MyNat.is_zero_zero as "is_zero_zero" in "Peano"
+"
+`is_zero_zero` is a proof of `is_zero 0 = True`.
+"
+
+LemmaDoc MyNat.is_zero_succ as "is_zero_succ" in "Peano"
+"
+`is_zero_succ a` is a proof of `is_zero (succ a) = False`.
+"
+
+LemmaDoc MyNat.succ_ne_zero as "succ_ne_zero" in "Peano"
+"
+`succ_ne_zero a` is a proof of `succ a ≠ 0`.
+"
+
+TacticDoc tauto "
+# Summary
+
+The `tauto` tactic proves pure logic goals, which can be resolved by truth tables.
+
+## Example
+
+If the goal is `True` then `tauto` will solve it.
+"
+
+/-- If $\operatorname{succ}(a)=\operatorname{succ}(b)$ then $a=b$. -/
+Statement succ_ne_zero (a : ℕ) : succ a ≠ 0 := by
+  Hint "Start with `intro h`."
+  intro h
+  Hint "We're going to change that `False` into `True`. Start by changing it into `is_zero (succ a)`."
+  Hint (hidden := true) "Try `rw [← is_zero_succ a]`."
+  rw [← is_zero_succ a]
+  rw [h]
+  rw [is_zero_zero]
+  tauto
+
+Conclusion
+"Let's now use these lemmas to prove
+"
