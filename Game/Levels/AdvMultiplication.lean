5f4a558f37229d23ee3c0622029e0e0dc204dd3c
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-10-28 15:24:49 +0100

    level 1

--- Game/Levels/AdvMultiplication.lean
+++ Game/Levels/AdvMultiplication.lean
@@ -1,16 +1,22 @@
-import Game.Levels.Multiplication
-import Game.Levels.AdvAddition
+import Game.Levels.AdvMultiplication.L01mul_le_mul_right
 
 World "AdvMultiplication"
 Title "Advanced Multiplication World"
 
 Introduction
 "
-Advanced Addition World proved various implications
-involving addition, such as `x + y = x → y = 0`. These lemmas
-are used to prove basic facts about ≤ in ≤ World. In
-Advanced Multiplication World we prove analogous
-facts about multiplication, such as `x * y = x → y = 1`,
-but only under the hypothesis `x ≠ 0`. This will prepare
+Advanced *Addition* World proved various implications
+involving addition, such as `x + y = 0 → x = 0` and `x + y = x → y = 0`.
+These lemmas were used to prove basic facts about ≤ in ≤ World.
+
+In Advanced Multiplication World we prove analogous
+facts about multiplication, such as `x * y = 1 → x = 1`, and
+`x * y = x → y = 1` (assuming `x ≠ 0` in the latter result). This will prepare
 us for Divisibility World.
+
+Multiplication World is more complex than Addition World. In the same
+way, Advanced Multiplication world is more complex than Advanced Addition
+World. One reason for this is that certain intermediate results are only
+true under the additional hypothesis that one of the variables is non-zero.
+This causes some unexpected extra twists.
 "
--- Game/Levels/AdvMultiplication/all_levels.lean
+++ Game/Levels/AdvMultiplication/all_levels.lean
@@ -1,19 +1,21 @@
-import Game.Levels.Multiplication
-import Game.Levels.LessOrEqual
+import Game.Levels.AdvMultiplication.L01mul_le_mul_right
 
 namespace MyNat
 
--- good level 1; used in the important `le_mul_right`
-lemma mul_le_mul_right (a b t : ℕ) (h : a ≤ b) : a * t ≤ b * t := by
-  cases h with d hd
-  use d * t
-  rw [hd, add_mul]
-  rfl
+-- -- good level 1; used in the important `le_mul_right`
+-- lemma mul_le_mul_right (a b t : ℕ) (h : a ≤ b) : a * t ≤ b * t := by
+--   cases h with d hd
+--   use d * t
+--   rw [hd, add_mul]
+--   rfl
 
 -- used in `le_mul_right`
 lemma mul_left_ne_zero (a b : ℕ) (h : a * b ≠ 0) : b ≠ 0 := by
-  by_contra hb
-  rw [hb, mul_zero] at h
+  apply mt _ h
+  have h1 : b = 0 → a * b = 0
+  · intro h2
+    rw [h2, mul_zero]
+    rfl
   tauto
 
 -- used in `le_mul_right`
@@ -26,13 +28,45 @@ lemma one_le_of_zero_ne (a : ℕ) (ha : a ≠ 0) : 1 ≤ a := by
     rfl
 
 -- `le_mul_right` is used in `if a ∣ x and x ≠ 0 then a ≤ x`, a key fact
--- for proving 2 is prime!
+-- for proving 2 is prime! It's also used in cd=1=>c=1, which Archie needs.
 lemma le_mul_right (a b : ℕ) (h : a * b ≠ 0) : a ≤ a * b := by
   apply mul_left_ne_zero at h
   apply one_le_of_zero_ne at h
   apply mul_le_mul_right 1 b a at h
   rw [one_mul, mul_comm] at h
   exact h
+
+-- needed, with le_mul_right, to prove cd=1=>c=1
+
+lemma le_one (a : ℕ) (ha : a ≤ 1) : a = 0 ∨ a = 1 := by
+  cases a with b
+  · left
+    rfl
+  · cases b with c
+    · right
+      rw [one_eq_succ_zero]
+      rfl
+    · cases ha with x hx
+      rw [succ_add, succ_add] at hx
+      rw [one_eq_succ_zero] at hx
+      apply succ_inj at hx
+      apply zero_ne_succ at hx
+      tauto
+
+-- Archie needs this
+lemma mul_right_eq_one (c d : ℕ) (h : c * d = 1) : c = 1 := by
+  have foo : c ≤ 1 := by
+    rw [← h]
+    apply le_mul_right
+    rw [h]
+    symm
+    apply zero_ne_succ
+  apply le_one at foo
+  cases foo with h0 h1
+  · rw [h0, zero_mul] at h
+    apply zero_ne_succ at h
+    tauto
+  exact h1
 
 -- used in `mul_ne_zero`, which is used in `mul_eq_zero`, which is used in `mul_left_cancel`
 lemma eq_succ_of_ne_zero (a : ℕ) (ha : a ≠ 0) : ∃ n, a = succ n := by
@@ -57,7 +91,7 @@ lemma mul_eq_zero (a b : ℕ) (h : a * b = 0) : a = 0 ∨ b = 0 := by
   have hab := mul_ne_zero a b
   tauto
 
--- very natural goal
+-- very natural goal; challenging. Also used in `mul_left_eq_self`, which Archie needs.
 lemma mul_left_cancel (a b c : ℕ) (ha : a ≠ 0) (h : a * b = a * c) : b = c := by
   induction b with d hd generalizing c -- this is what trips everyone up
   · rw [mul_zero] at h
@@ -77,40 +111,8 @@ lemma mul_left_cancel (a b c : ℕ) (ha : a ≠ 0) (h : a * b = a * c) : b = c :
       rfl
       exact h
 
-lemma le_one (a : ℕ) (ha : a ≤ 1) : a = 0 ∨ a = 1 := by
-  cases a with b
-  · left
-    rfl
-  · cases b with c
-    · right
-      rw [one_eq_succ_zero]
-      rfl
-    · cases ha with x hx
-      rw [succ_add, succ_add] at hx
-      rw [one_eq_succ_zero] at hx
-      apply succ_inj at hx
-      apply zero_ne_succ at hx
-      tauto
-
 -- Archie needs this
-example (c d : ℕ) (h : c * d = 1) : c = 1 := by
-  have foo : c ≤ 1 := by
-    rw [← h]
-    apply le_mul_right
-    rw [h]
-    symm
-    apply zero_ne_succ
-  apply le_one at foo
-  cases foo with h0 h1
-  · rw [h0, zero_mul] at h
-    apply zero_ne_succ at h
-    tauto
-  exact h1
-
-
-
--- Archie needs this
-example (a b : ℕ) (h : b = a * b) (hb : b ≠ 0) : a = 1 := by
+lemma mul_left_eq_self (a b : ℕ) (h : b = a * b) (hb : b ≠ 0) : a = 1 := by
   rw [mul_comm] at h
   nth_rewrite 1 [← mul_one b] at h
   apply mul_left_cancel at h
