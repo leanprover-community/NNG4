e47884d92c16ff575fba9edbf3d1f7986c7b179e
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-12-18 16:15:52 +0000

    add docstring for !=

--- Game/Levels/Implication/L08ne.lean
+++ Game/Levels/Implication/L08ne.lean
@@ -31,6 +31,23 @@ Statement (x y : ℕ) (h1 : x = y) (h2 : x ≠ y) : False := by
   apply h2 at h1
   exact h1
 
+DefinitionDoc Ne as "≠" "
+  `a ≠ b` is *notation* for `(a = b) → False`.
+
+  The reason this is mathematically
+  valid is that if `P` is a true-false statement then `P → False`
+  is the logical opposite of `P`. Indeed `True → False` is false,
+  and `False → False` is true!
+
+  The upshot of this is that use can treat `a ≠ b` in exactly
+  the same way as you treat any implication `P → Q`. For example,
+  if your *goal* is of the form `a ≠ b` then you can make progress
+  with `intro h`, and if you have a hypothesis `h` of the
+  form `a ≠ b` then you can `apply h at h1` if `h1` is a proof
+  of `a = b`.
+
+"
+
 NewDefinition Ne
 
 Conclusion "Remember, `x ≠ y` is *notation* for `x = y → False`."
