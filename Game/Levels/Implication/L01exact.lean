0e7a8d4ca20fb7a2d055111bcad62d63a3b173c9
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-12-18 15:52:42 +0000

    clarify which goal we're talking about

--- Game/Levels/Implication/L01exact.lean
+++ Game/Levels/Implication/L01exact.lean
@@ -52,5 +52,5 @@ unused variable `h2` [linter.unusedVariables]
 but I want `h2` to be there.
 -/
 Statement (x y z : ℕ) (h1 : x + y = 37) (h2 : 3 * x + z = 42) : x + y = 37 := by
-  Hint "The goal is one of our hypotheses. Solve the goal by executing `exact h1`."
+  Hint "The goal in this level is one of our hypotheses. Solve the goal by executing `exact h1`."
   exact h1
