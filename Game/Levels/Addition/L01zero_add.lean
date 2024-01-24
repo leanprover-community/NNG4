2112b6c0ac3914466af1d38a62559a514ffc062e
Abram <115729028+Not-Abram@users.noreply.github.com>
2024-01-24 11:28:16 -0600

    Return to typewriter mode suggestion

--- Game/Levels/Addition/L01zero_add.lean
+++ Game/Levels/Addition/L01zero_add.lean
@@ -28,6 +28,9 @@ will ask us to show that if `0 + d = d` then `0 + succ d = succ d`. Because
 `0` and successor are the only way to make numbers, this will cover all the cases.
 
 See if you can do your first induction proof in Lean.
+
+By the way, if you are still in the editor mode from the last world, you can swap
+back to typewriter mode by clicking the `>_` button.
 "
 
 /--
