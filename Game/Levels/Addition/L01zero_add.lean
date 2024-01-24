b248e1cca061cf6b7ea9388c94a89c8fd93654b2
Steven Clontz <steven.clontz@gmail.com>
2024-01-24 11:33:31 -0600

    add parentheses, consistent language around "modes"

--- Game/Levels/Addition/L01zero_add.lean
+++ Game/Levels/Addition/L01zero_add.lean
@@ -29,8 +29,8 @@ will ask us to show that if `0 + d = d` then `0 + succ d = succ d`. Because
 
 See if you can do your first induction proof in Lean.
 
-By the way, if you are still in the editor mode from the last world, you can swap
-back to typewriter mode by clicking the `>_` button.
+(By the way, if you are still in the \"Editor mode\" from the last world, you can swap
+back to \"Typewriter mode\" by clicking the `>_` button in the top right.)
 "
 
 /--
