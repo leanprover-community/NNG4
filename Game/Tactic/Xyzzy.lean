d6bf5686bc200f402301eef37354031da43e72c2
Jon Eugster <eugster.jon@gmail.com>
2024-03-14 10:38:41 +0100

    rename iGiveUpAx to xyzzyAxiom

--- Game/Tactic/Xyzzy.lean
+++ Game/Tactic/Xyzzy.lean
@@ -3,11 +3,11 @@ import Lean
 universe u
 
 @[never_extract]
-axiom iGiveUpAx (α : Sort u) (synthetic := false) : α
+axiom xyzzyAxiom (α : Sort u) (synthetic := false) : α
 
 /--
 `xyzzy` is an ancient magic word, believed to be the root of the
 modern word `sorry`. The game won't complain - or notice - if you
 prove anything with `xyzzy`.
 -/
-macro "xyzzy" : tactic => `(tactic| exact @iGiveUpAx _ false)
+macro "xyzzy" : tactic => `(tactic| exact @xyzzyAxiom _ false)
