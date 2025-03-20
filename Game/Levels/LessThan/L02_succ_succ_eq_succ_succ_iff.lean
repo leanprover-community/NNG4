83703d8a6af3f5171d9e107f8b4caa6001af4c94
Kent Van Vels <kvanvels@protonmail.com>
2025-03-20 09:29:56 -0400

    fixed the naming issue for succ_succ_eq_succ_succ_iff

--- Game/Levels/LessThan/L02_succ_succ_eq_succ_succ_iff.lean
+++ Game/Levels/LessThan/L02_succ_succ_eq_succ_succ_iff.lean
@@ -13,12 +13,8 @@ tactic with iff statements.  Before this level, you used `rw` to
 substitute *equations* into the goal or into other expressions.  The `rw` tactic
 can be used to rewrite equivalent *propositions* into each other in the same way."
 
-/-- YADDA We don't really want this as a theorem do we?  TODO: Check how the other intro
-levels were handled. -/
-TheoremDoc MyNat.succ_succ_eq_succ_succ_iff as "succ_succ_eq_succ_succ_iff" in "<"
-
 /--Equal numbers have equal successors of successors and vice versa.-/
-Statement succ_succ_eq_succ_succ_iff (a b : ℕ) :
+Statement (a b : ℕ) :
   succ (succ a) = succ (succ b) ↔ a = b := by
   Hint "To strip of one pair of `succ`'s, you can use `rw [succ_eq_succ_iff]`."
   rw [succ_eq_succ_iff]
