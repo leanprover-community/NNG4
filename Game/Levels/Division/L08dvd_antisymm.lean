ff96e2c7196596092acbe5709b2b0119c010fb11
ftm-2005 <fyk@andrew.cmu.edu>
2025-05-22 19:24:40 +0300

    Update L08dvd_antisymm.lean

--- Game/Levels/Division/L08dvd_antisymm.lean
+++ Game/Levels/Division/L08dvd_antisymm.lean
@@ -1,5 +1,5 @@
 
-/--import Game.Levels.AdvMultiplication
+import Game.Levels.AdvMultiplication
 import Game.MyNat.Division
 
 World "Division"
@@ -20,9 +20,19 @@ Introduction
 TheoremDoc MyNat.dvd_antisymm as "dvd_antisymm" in "∣"
 
 Statement dvd_antisymm
-    (a b : ℕ) (h : a ∣ b ∧ b ∣ a): a = b := by
-  Hint "You will need to expand what `h1` and `h2` actually mean. You may find `cases` helpful."
-  cases h with h1 h2
-  cases h1 with m hm
-  cases h2 with n hn
-  sorry--/
+    (a b : ℕ) (h1 : a ∣ b) (h2 : b ∣ a): a = b := by
+  cases h1 with m hm        -- b = a * m
+  cases h2 with n hn        -- a = b * n
+  rw [hn] at hm             -- a = (a * m) * n
+  cases b with b'
+  rw [zero_mul] at hn     -- a = 0
+  rw [hn]
+  rfl
+  rw [mul_assoc] at hm    -- a = a * (m * n)
+  symm at hm              -- a * (m * n) = a
+  have ha : succ b' ≠ 0 := succ_ne_zero b'
+  apply mul_right_eq_self at hm    -- concludes m * n = 1
+  apply mul_right_eq_one at hm           -- from m * n = 1 → m = 1 ∧ n = 1
+  rw [hm, mul_one] at hn
+  exact hn
+  exact ha
