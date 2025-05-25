fa6dec9639adf6afa189f9ec37588164fa6f8343
coolujain <143888770+coolujain@users.noreply.github.com>
2025-05-25 21:55:44 +0300

    Create L11dvd_mul_both.lean

---
+++ Game/Levels/Division/L11dvd_mul_both.lean
@@ -0,0 +1,29 @@
+import Game.Levels.Multiplication
+import Game.Levels.AdvMultiplication
+import Game.MyNat.Division
+import Game.MyNat.PeanoAxioms
+
+World "Division"
+Level 11
+Title ""
+
+TheoremTab "∣"
+
+namespace MyNat
+
+Introduction
+"
+  In this level, we will prove that if a | b  then a*c | b*c for all a,b,c ∈ ℕ.
+"
+/-- `dvd_mul_both a b c` is a proof that if `a | b` then `a*c | b*c` .-/
+TheoremDoc MyNat.dvd_mul_both as "dvd_mul_both" in "∣"
+
+Statement dvd_mul_both
+    (a b c  : ℕ) (h1 : a ∣ b) : ( a*c ∣ b*c ) := by
+    cases h1 with k h11
+    use k
+    rw [h11]
+    rw [mul_assoc]
+    rw [mul_comm k c]
+    rw [<- mul_assoc]
+    rfl
