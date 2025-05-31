2248ad969e692792ea184a9256608e4b06f84cb9
ftm-2005 <fyk@andrew.cmu.edu>
2025-05-31 22:54:43 +0300

    Update Division.lean

--- Game/Levels/Division.lean
+++ Game/Levels/Division.lean
@@ -4,9 +4,9 @@ import Game.Levels.Division.L03dvd_zero
 import Game.Levels.Division.L04dvd_one
 import Game.Levels.Division.L05dvd_trans
 import Game.Levels.Division.L06zero_dvd
-import Game.Levels.Division.L07dvd_ls
+import Game.Levels.Division.L07dvd_le
 import Game.Levels.Division.L08dvd_antisymm
-import Game.Levels.Division.L09dvd_add
+import Game.Levels.Division.L09dvd_add_right
 import Game.Levels.Division.L10dvd_mul_right
 import Game.Levels.Division.L11dvd_mul_both
 import Game.Levels.Division.L12dvd_not_eq
