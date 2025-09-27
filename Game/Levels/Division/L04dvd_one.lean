0496a4e0b6d8b8a3c7261e74ca9f5f5ee73c5053
ftm-2005 <fyk@andrew.cmu.edu>
2025-09-27 12:10:57 +0300

    Update Game/Levels/Division/L04dvd_one.lean
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- Game/Levels/Division/L04dvd_one.lean
+++ Game/Levels/Division/L04dvd_one.lean
@@ -23,7 +23,7 @@ Statement dvd_one
     (n : ℕ) : (h:  n ∣ 1 ) → n = 1 := by
     Hint "Start with `intro`"
     intro
-    Hint "Now try cases h with k h1"
+    Hint "Now try `cases h with k h1`"
     cases h with k h1
     symm at h1
     apply mul_right_eq_one at h1
