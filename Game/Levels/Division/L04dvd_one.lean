34273afda49ef11552d433710319b5d708414291
ftm-2005 <fyk@andrew.cmu.edu>
2025-09-27 12:10:47 +0300

    Update Game/Levels/Division/L04dvd_one.lean
    Co-authored-by: Jon Eugster <eugster.jon@gmail.com>

--- Game/Levels/Division/L04dvd_one.lean
+++ Game/Levels/Division/L04dvd_one.lean
@@ -21,7 +21,7 @@ TheoremDoc MyNat.dvd_one as "dvd_one" in "∣"
 
 Statement dvd_one
     (n : ℕ) : (h:  n ∣ 1 ) → n = 1 := by
-    Hint "Start with intro"
+    Hint "Start with `intro`"
     intro
     Hint "Now try cases h with k h1"
     cases h with k h1
