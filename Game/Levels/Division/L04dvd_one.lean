6d5f2007bb4f5ac1fc56e4d85878ad50d7f334a3
ftm-2005 <fyk@andrew.cmu.edu>
2025-10-12 12:24:13 +0300

    Update L04dvd_one.lean added (hidden := true)

--- Game/Levels/Division/L04dvd_one.lean
+++ Game/Levels/Division/L04dvd_one.lean
@@ -13,7 +13,7 @@ namespace MyNat
 
 Introduction
 "
-  In this level, we will prove that if n ∣ 1 then n = 1 for all n ∈ ℕ. In other words,
+  In this level, we will prove that if `n ∣ 1` then `n = 1` for all n ∈ ℕ. In other words,
   only one can divide one.
 "
 /-- `dvd_one n` is a proof that if `n | 1` then `n = 1`.-/
@@ -21,9 +21,9 @@ TheoremDoc MyNat.dvd_one as "dvd_one" in "∣"
 
 Statement dvd_one
     (n : ℕ) : (h:  n ∣ 1 ) → n = 1 := by
-    Hint "Start with `intro`"
+    Hint (hidden := true) "Start with `intro`"
     intro
-    Hint "Now try `cases h with k h1`"
+    Hint (hidden := true) "Now try `cases h with k h1`"
     cases h with k h1
     symm at h1
     apply mul_right_eq_one at h1
