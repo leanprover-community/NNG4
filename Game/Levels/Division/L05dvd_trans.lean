2341c2d475acd69a00f60419653f9fa89a9555af
ftm-2005 <fyk@andrew.cmu.edu>
2025-10-12 12:25:03 +0300

    Update L05dvd_trans.lean added `` and (hidden := true)

--- Game/Levels/Division/L05dvd_trans.lean
+++ Game/Levels/Division/L05dvd_trans.lean
@@ -13,7 +13,7 @@ namespace MyNat
 
 Introduction
 "
-  In this level, we will prove that if a | b and b | c then a | c for all a,b,c ∈ ℕ. In other words,
+  In this level, we will prove that if `a | b` and `b | c` then `a | c` for all a,b,c ∈ ℕ. In other words,
   we will prove the transitivity of division.
 "
 /-- `dvd_trans a b c` is a proof that if `a | b` and `b | c `then `a | c`.-/
@@ -25,7 +25,7 @@ Statement dvd_trans
   cases hbc with m hm
   cases hab with n hn
   rw[hn] at hm
-  Hint "Now, since we are looking to show `a ∣ c`, which is an existience hypothesis, the `use`
+  Hint (hidden := true) "Now, since we are looking to show `a ∣ c`, which is an existience hypothesis, the `use`
   tactic would be a good choice."
   use (n * m)
   rw [← mul_assoc]
